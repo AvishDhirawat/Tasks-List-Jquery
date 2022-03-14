e2d39dfdb8198ea5e600251038aff253c6195661
Avish Dhirawat <31956212+Avishdhirawat0@users.noreply.github.com>
2022-03-14 15:44:54 +0530

    Major Changes : Added Quotes using AJAX

--- js/tasks.js
+++ js/tasks.js
@@ -1,5 +1,21 @@
+$(function(){
+	var quoteapi = "https://api.quotable.io/random";
+	
+	$.getJSON(quoteapi).done(function(data){
+		console.log(data);
+		var quote = $(".random-quote");
+		quote.empty();
+		quote.append("<p style='font-family: Anton; font-size:20px; color:green'>" + data.content + "</p>");
+		quote.append("<span style='float: right;font-family: Anton; color:green'>" + "--- " + data.author + "</span>");
+		// var author = $("#author");
+		// author.empty();
+		// author.append("<br><p> " + data.author + "</p>");
 
-$(".todoinput").keypress(function(event) {
+	});
+});
+
+
+$(".taskinput").keypress(function(event) {
 	if(event.which === 13) {
 		if($(this).val()==="") return;
 		$("ul").prepend("<li><span class='left'><i class='fa fa-trash'></i></span> <span class='text'> "+$(this).val()+"</span><span class='right'><i class='fa fa-pencil'></li>"); // For Dynamic code Completion
